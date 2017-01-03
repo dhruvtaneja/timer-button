2709878311aed83e879923b9d1f7a147c7d491b5
Dhruv Taneja <dhruvtaneja1209@gmail.com>
2017-01-03 23:56:29 +0530

    Replace all the views with timer button

--- app/src/main/java/dhruv/com/butim/MainActivity.java
+++ app/src/main/java/dhruv/com/butim/MainActivity.java
@@ -1,100 +1,36 @@
 package dhruv.com.butim;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 
-import java.util.concurrent.TimeUnit;
+import dhruv.com.butim.timerbutton.TimerButton;
 
 public class MainActivity extends AppCompatActivity {
 
-	private static final long MILLIS_IN_FUTURE = 10000L;
-	private static final long COUNT_DOWN_INTERVAL = 1000L;
+    private static final long MILLIS_IN_FUTURE = 10000L;
 
-	private Button mOverButton;
-	private Button mButton;
-	private View mOverView;
-	private ScaleAnimation mScaleAnimation;
-	private CountDownTimer mTimer;
+    private TimerButton mTimerButton;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-		mButton = (Button) findViewById(R.id.timer_base_button);
-		mOverButton = (Button) findViewById(R.id.text_button);
-		Button endAnimButton = (Button) findViewById(R.id.end_anim_button);
-		mOverView = findViewById(R.id.over_view);
+        Button endAnimButton = (Button) findViewById(R.id.end_anim_button);
+        mTimerButton = (TimerButton) findViewById(R.id.timer_button);
+        mTimerButton.setStaticText(getString(R.string.send_otp));
+        mTimerButton.setDynamicText(R.string.resend_otp_formatted);
+        mTimerButton.setOnAnimationCompleteText(getString(R.string.resend_otp));
+        mTimerButton.setDuration(MILLIS_IN_FUTURE);
 
-		mScaleAnimation = new ScaleAnimation(0.0f, 1.0f, 1.0f, 1.0f);
-		mScaleAnimation.setInterpolator(new LinearInterpolator());
-		mScaleAnimation.setDuration(MILLIS_IN_FUTURE);
-		mButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mOverView.startAnimation(mScaleAnimation);
-				startTimer();
-			}
-		});
-
-		endAnimButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!mScaleAnimation.hasEnded()) {
-					mOverView.clearAnimation();
-                    if (mTimer != null) {
-                        mTimer.onFinish();
-                        mTimer.cancel();
-                    }
-				}
-			}
-		});
-
-		mScaleAnimation.setAnimationListener(new Animation.AnimationListener() {
-			@Override
-			public void onAnimationStart(Animation animation) {
-				mOverView.setVisibility(View.VISIBLE);
-                mOverButton.setVisibility(View.VISIBLE);
-				mButton.setEnabled(false);
-			}
-
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				mOverView.setVisibility(View.GONE);
-                mOverButton.setVisibility(View.GONE);
-				mButton.setEnabled(true);
-			}
-
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-
-			}
-		});
-	}
-
-	private void startTimer() {
-		mTimer = new CountDownTimer(MILLIS_IN_FUTURE, COUNT_DOWN_INTERVAL) {
-			@Override
-			public void onTick(long millisUntilFinished) {
-				int left = (int) (millisUntilFinished / TimeUnit.SECONDS.toMillis(1));
-				String formattedString = String.format(
-						getString(R.string.resend_otp_formatted), left);
-				mOverButton.setText(formattedString);
-				mButton.setText(formattedString);
-			}
-
-			@Override
-			public void onFinish() {
-				mButton.setText(R.string.resend_otp);
-			}
-		};
-		mTimer.start();
-	}
+        endAnimButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTimerButton.reset();
+            }
+        });
+    }
 }
